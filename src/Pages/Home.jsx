72b4bc170c9fb2b2c67012d09ef4c930c3666ec8
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2025-09-13 23:38:28 +0530

    added button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
+    
     <div>
-      <Link to='signup'>
-      </Link>
-      <div className="">
-        <div>
-          <h1>Welcome to Study Notion</h1>
-          <p>Your ultimate study companion</p>
-        </div>
+      <Link  to = {'/signup'}>Sign Up</Link>
+
+      <div className='text-4xl text-center font-semibold mt-7'>
+         Empower your future with 
+         <HighlightText text={"Coding Skills"} />
+      </div>
+      <div>
         
       </div>
     </div>
+    
   )
 }
 
---
+++ src/components/core/Homepage/HighlightText.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HighlightText = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HighlightText
