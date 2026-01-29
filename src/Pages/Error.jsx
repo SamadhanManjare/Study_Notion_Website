e0826c9bd1c43548392fc9f369f1de571c71da17
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2026-01-29 18:30:20 +0530

    Error Handler

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Error = () => {
   return (
+    // Error page for unmatched routes
     <div className='flex justify-center items-center text-3xl text-white'>
       Error - 404 Not found
     </div>
