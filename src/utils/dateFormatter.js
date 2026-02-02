97950f9c0c54b9dbaf68c69c86709daf1a11adac
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2026-02-02 20:18:50 +0530

    date formatter

--- src/utils/dateFormatter.js
+++ src/utils/dateFormatter.js
@@ -1,4 +1,5 @@
 export const formattedDate = (date) => {
+  // Formats a date into "Month Day, Year" format
     return new Date(date).toLocaleDateString("en-US", {
       month: "long",
       day: "numeric",
